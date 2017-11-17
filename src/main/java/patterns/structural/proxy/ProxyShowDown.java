365cd720bd60dcb54b34225c30229a568fbeb596
Natalia <Mykola_Zomchak@epam.com>
2017-11-17 17:27:07 +0200

    Add proxy

---
+++ src/main/java/patterns/structural/proxy/ProxyShowDown.java
@@ -0,0 +1,15 @@
+package patterns.structural.proxy;
+
+public class ProxyShowDown {
+
+    public static void main(String[] args) {
+        Image image = new ProxyImage("testImage");
+        System.out.println("Created proxy");
+        System.out.println("Some text before displaying te image");
+        image.display();
+        System.out.print("\n");
+
+        image.display();
+    }
+
+}
