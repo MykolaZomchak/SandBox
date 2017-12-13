e1cb1e7c518ca2ba6723f0bb9077db14ba2e1c32
Natalia <Mykola_Zomchak@epam.com>
2017-12-13 16:58:13 +0200

    Improve Proxy demo

--- src/main/java/patterns/structural/proxy/ProxyShowDown.java
+++ src/main/java/patterns/structural/proxy/ProxyShowDown.java
@@ -5,6 +5,8 @@ public class ProxyShowDown {
     public static void main(String[] args) {
         Image image = new ProxyImage("testImage");
         System.out.println("Created proxy");
+        System.out.println("Image file name: " + image.getName());
+        image.setName("new name");
         System.out.println("Some text before displaying the image");
         image.display();
         System.out.print("\n");
